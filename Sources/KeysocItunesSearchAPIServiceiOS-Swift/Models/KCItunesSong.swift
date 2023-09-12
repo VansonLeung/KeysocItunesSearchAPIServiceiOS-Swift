0a8a6d0b2de847059ba9063a5de46c95b25cde1b
Vanson Wing Leung <vanson@vanportdev.com>
2023-09-12 23:40:37 +0800

    [FIX] Update song model

--- Sources/KeysocItunesSearchAPIServiceiOS-Swift/Models/KCItunesSong.swift
+++ Sources/KeysocItunesSearchAPIServiceiOS-Swift/Models/KCItunesSong.swift
@@ -10,19 +10,35 @@ import Foundation
 
 // Define the Song struct to match the API response structure
 public struct KCItunesSong: Codable {
-    public let trackName: String?
+    public let wrapperType: String?
+    public let kind: String?
+    public let artistId: Int?
+    public let collectionId: Int?
+    public let trackId: Int?
     public let artistName: String?
     public let collectionName: String?
-    public let previewUrl: URL? // Use URL for URL strings
-
-    // Add other properties as needed
-
-    // Define CodingKeys to map JSON keys to struct properties
-    private enum CodingKeys: String, CodingKey {
-        case trackName
-        case artistName
-        case collectionName
-        case previewUrl = "previewUrl"
-        // Add other cases for properties not matching the JSON keys
-    }
+    public let trackName: String?
+    public let collectionCensoredName: String?
+    public let trackCensoredName: String?
+    public let artistViewUrl: URL?
+    public let collectionViewUrl: URL?
+    public let trackViewUrl: URL?
+    public let previewUrl: URL?
+    public let artworkUrl30: URL?
+    public let artworkUrl60: URL?
+    public let artworkUrl100: URL?
+    public let collectionPrice: Double?
+    public let trackPrice: Double?
+    public let releaseDate: String?
+    public let collectionExplicitness: String?
+    public let trackExplicitness: String?
+    public let discCount: Int?
+    public let discNumber: Int?
+    public let trackCount: Int?
+    public let trackNumber: Int?
+    public let trackTimeMillis: Int?
+    public let country: String?
+    public let currency: String?
+    public let primaryGenreName: String?
+    public let isStreamable: Bool?
 }
